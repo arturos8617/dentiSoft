7b8ba655ac971fe75bcc5131c4810cc7f146e765
arturos8617 <arturo.sanchez7480@alumnos.udg.mx>
2025-06-13 17:17:58 -0600

    string the user id optionality for the fecha de expiracion inviteregisterapi can be accessed without authentication

--- dentisoft/core/api/views.py
+++ dentisoft/core/api/views.py
@@ -1,5 +1,6 @@
 from rest_framework import status, viewsets
 from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
 from rest_framework.views import APIView
 
 from core.api.serializers import InvitacionUsuarioSerializer, InviteRegisterSerializer
@@ -23,8 +24,9 @@ class InvitacionUsuarioViewSet(viewsets.ModelViewSet):
 
 
 class InviteRegisterAPIView(APIView):
+    permission_classes = [AllowAny]
     def post(self, request):
         serializer = InviteRegisterSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.save()
-        return Response({"id": user.id, "email": user.email}, status=status.HTTP_201_CREATED)
+        return Response({"id": str(user.id), "email": user.email}, status=status.HTTP_201_CREATED)
